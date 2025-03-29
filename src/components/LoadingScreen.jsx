1bcb748dd79841f756a139208ee07ef153c57306
Gee Brain <gozkybrain@gmail.com>
2025-03-29 11:47:21 +0100

    Loading Screen Fixed

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -3,9 +3,14 @@ import '../styles/LoadingScreen.css';
 
 const LoadingScreen = ({ message, subMessage }) => (
   <div className="loading-screen">
+    <div className="loading-grid">
+      <div className="loading-box"></div>
+      <div className="loading-box"></div>
+      <div className="loading-box"></div>
+      <div className="loading-box"></div>
+    </div>
     <div className="loading-message">{message}</div>
     {subMessage && <div className="loading-submessage">{subMessage}</div>}
-    <div className="loading-spinner"></div>
   </div>
 );
 
